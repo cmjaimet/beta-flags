0ec8a4c310d011f56540d3d12b64d0fd478b5657
Charles Jaimet <charles@calmseamedia.com>
2018-10-15 07:58:09 -0400

    Unit test Admin
    Still not complete coverage

--- tests/test-admin.php
+++ tests/test-admin.php
@@ -31,21 +31,66 @@ class TestAdmin extends \WP_UnitTestCase {
 		$output = ob_get_contents();
 		ob_end_clean();
 		$this->assertContains( '<h1>Beta Flags</h1>', $output );
+		$this->assertContains( 'sticky_video', $output );
+		$nonce_value = wp_create_nonce( 'betaflagsnonce' );
+		$this->assertContains( '<input type="hidden" id="betaflagsnonce" name="betaflagsnonce" value="' . $nonce_value . '" />', $output );
 	}
 
-	function xtest_list_flags() {
+	function test_list_flags() {
+		ob_start();
+		$this->admin->settings_page();
+		$output = ob_get_contents();
+		ob_end_clean();
+		$this->assertContains( '<th>Enabled</th>', $output );
+		$this->assertContains( '<td><input type="checkbox" name="flags[sticky_video][ab_test]" value="1"  /></td>', $output );
 	}
 
-	function xtest_get_flag_setting() {
+	function test_get_flag_setting() {
+		$this->assertEquals( $this->admin->get_flag_setting( 'new_sidebar', 'ab_test' ), 0 );
+		$this->assertEquals( $this->admin->get_flag_setting( 'new_sidebar', 'enabled' ), 1 );
+		$this->assertEquals( $this->admin->get_flag_setting( 'sticky_video', 'ab_test' ), 0 );
+		$this->assertEquals( $this->admin->get_flag_setting( 'sticky_video', 'enabled' ), 0 );
 	}
 
-	function xtest_form_submit() {
+	function test_form_submit() {
+		$this->assertTrue( true ); // placeholder to prevent PHPUnit warnings until test written
 	}
 
-	function xtest_form_validate() {
+	function test_form_validate() {
+		$this->assertEquals( $this->admin->form_validate(), 'You are not authorized to perform that action (E353)' );
+		$_POST[ 'betaflagsnonce' ] = 'baddata';
+		$this->assertEquals( $this->admin->form_validate(), 'You are not authorized to perform that action (E314)' );
+		$nonce_value = wp_create_nonce( 'betaflagsnonce' );
+		$_POST[ 'betaflagsnonce' ] = $nonce_value;
+		$this->assertEquals( $this->admin->form_validate(), '' );
+		$user_id = $this->make_user( 'author' );
+		wp_set_current_user( $user_id );
+		$this->assertEquals( $this->admin->form_validate(), 'You are not authorized to perform that action (E451)' );
 	}
 
-	function xtest_get_flag_data() {
+	function test_get_flag_data() {
+		$flag_data = $this->admin->get_flag_data();
+		$this->assertEquals( 'Fred Page', $flag_data->redesign_v109->author );
+		// $json_plugin = FF_PLUGIN_PATH . 'data/beta-flags.json';
+		// $json_theme = get_template_directory() . '/beta-flags.json';
+		// create a file in theme
+		// copy( $json_plugin, $json_theme );
+		// $flag_data = $this->admin->get_flag_data();
+		// $this->assertEquals( 'Fred Page', $flag_data->redesign_v109->author );
+		// $json_text = file_get_contents( $json_theme );
+		// $json_text = str_replace( '"Fred Page"', '"Allison Page"', $json_text );
+		// file_put_contents( $json_theme, $json_text );
+		// $flag_data = $this->admin->get_flag_data();
+		// $this->assertEquals( 'Allison Page', $flag_data->redesign_v109->author );
+		// alter file in theme
+		// $json_text = str_replace( '"Allison Page"', '"Allison Page",', $json_text );
+		// file_put_contents( $json_theme, $json_text );
+		// $flag_data = $this->admin->get_flag_data();
+		// $this->assertEquals( 'Configuration file does not contain valid JSON', $flag_data );
+		// delete file in theme
+		// alter file in plugin
+		// delete file in plugin
+		// delete file in plugin
 	}
 
 	function tearDown() {
